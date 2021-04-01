67904cb6939e35606aea62911aa671ddd6f5eac9
Royal <thaparoyal17@gmail.com>
2021-04-01 21:58:06 +0545

    final commit

--- script.js
+++ script.js
@@ -18,30 +18,39 @@
 let beats = {
   65: {
     beat: new Beat("./assets/Piano Chord 331.mp3"),
+    button: new Button("#00fffe", 65),
   },
   83: {
     beat: new Beat("./assets/Piano Chord 209.mp3"),
+    button: new Button("#00fffe", 83),
   },
   68: {
     beat: new Beat("./assets/Piano Chord 208.mp3"),
+    button: new Button("#00fffe", 68),
   },
   70: {
     beat: new Beat("./assets/Drum Sticks Hit 3.mp3"),
+    button: new Button("#FF00FF", 70),
   },
   71: {
     beat: new Beat("./assets/Drum Snare Roll.mp3"),
+    button: new Button("#FF00FF", 71),
   },
   72: {
     beat: new Beat("./assets/PREL Musical 57.mp3"),
+    button: new Button("#FF00FF", 72),
   },
   74: {
     beat: new Beat("./assets/Cymbal Suspended 2.mp3"),
+    button: new Button("#FF00FF", 74),
   },
   75: {
     beat: new Beat("./assets/Musical Compos 33.mp3"),
+    button: new Button("#FFFFFF", 75),
   },
   76: {
     beat: new Beat("./assets/Musical Orches 4.mp3"),
+    button: new Button("#FFFFFF", 76),
   },
 };
 
@@ -50,10 +59,10 @@ let beats = {
  * HINT: use the keyCode
  */
 triggerBeat = (event) => {
-  const keyCode = event.keyCode;
-  if (keyCode in beats) {
-    let keyPress = beats[keyCode];
-    keyPress.beat.play();
+  let code = event.keyCode;
+  if (code in beats) {
+    beats[code].button.select();
+    beats[code].beat.play();
   }
 };
 
--- utils.js
+++ utils.js
@@ -6,9 +6,8 @@
 class Beat {
   constructor(audioSrc) {
     this.audio = new Audio(audioSrc);
-    console.log(this.audio);
-    this.audio.play();
   }
+
   play = () => {
     this.audio.currentTime = 0;
     this.audio.play();
@@ -19,20 +18,38 @@ class Beat {
  * Button class that keeps track of the button color based on a press
  */
 class Button {
-  constructor(color, keyCode) {}
+  constructor(color, keyCode) {
+    this.element = document.getElementById(keyCode);
+    this.keyCode = keyCode;
+    this.color = color;
+    this.setButtonColorInHTML();
+    this.createTransitionEndListener();
+  }
 
   /**
    * Set the button color based on color specified
    */
-  setButtonColorInHTML = () => {};
-
+  setButtonColorInHTML = () => {
+    this.element.style.borderColor = this.color;
+  };
+  createTransitionEndListener = () => {
+    this.element.addEventListener("transitionend", () => {
+      this.deselect();
+    });
+  };
   /**
    * Select function to set the background color and boxShadow
    */
-  select = () => {};
+  select = () => {
+    this.element.style.backgroundColor = this.color;
+    this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`;
+  };
 
   /**
    * Deselect function to reset background color and boxShadow
    */
-  deselect = () => {};
+  deselect = () => {
+    this.element.style.backgroundColor = "transparent";
+    this.element.style.boxShadow = `none`;
+  };
 }
